3f2d88eb3436686769342a892f5fe3896e540ef0
SelvyaSyalsabilla <syabillselvya@gmail.com>
2023-10-04 12:23:00 +0700

    new file:   application/views/partials/footer.php new file:   application/views/partials/head.php new file:   application/views/partials/navbar.php

---
+++ application/views/partials/navbar.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<nav class="navbar">
+		<a href="<?= site_url() ?>">Home</a>
+
+		<a href="<?= site_url('buku/list') ?>">Daftar Buku <i class="bi bi-book"></i></a>
+
+		<a href="<?= site_url('buku/add') ?>">Tambah Buku <i class="bi bi-database-add"></i></a>
+		
+		<a href="<?= site_url('buku/cari') ?>" style="margin-left: auto;"><i class="bi bi-search"></i> Cari</a>
+	</nav>
+</div>
